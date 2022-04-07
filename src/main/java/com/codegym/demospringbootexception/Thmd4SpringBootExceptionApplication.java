b980171291f5e1ce5cd29700156bcee78414a428
truonghungcuong <truonghungcuong12012001@gmail.com>
2022-04-07 10:42:34 +0700

    first commit

---
+++ src/main/java/com/codegym/demospringbootexception/Thmd4SpringBootExceptionApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.demospringbootexception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Thmd4SpringBootExceptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Thmd4SpringBootExceptionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codegym/demospringbootexception/Thmd4SpringBootExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.demospringbootexception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Thmd4SpringBootExceptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
